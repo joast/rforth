a6f2826cd6bb8071ce9a9621903720c98a88b09c
Rick Ohnemus <rick_ohnemus@acm.org>
2020-07-09 08:15:02 -0700

    fixed a bunch of rubocop complaints (place holders still need to be replaced)

--- src/rforth.rb
+++ src/rforth.rb
@@ -1,10 +1,16 @@
+# frozen_string_literal: true
+
+# rubocop:disable Metrics/ClassLength
+# rubocop:disable Metrics/MethodLength
+# rubocop:disable Metrics/ModuleLength
+
 require 'pp'
 
+# place holder
 module PrimitiveWords
-
   def dup(name)
     if @stack.empty?
-      $stderr.puts "#{name} stack underflow"
+      $stderr.print "#{name} stack underflow\n"
     else
       @stack << @stack.last
     end
@@ -16,7 +22,7 @@ module PrimitiveWords
 
   def drop(name)
     if @stack.empty?
-      $stderr.puts "#{name} stack underflow"
+      $stderr.print "#{name} stack underflow\n"
     else
       @stack.pop
     end
@@ -103,9 +109,9 @@ module PrimitiveWords
 
   def dot(name)
     if @stack.empty?
-      $stderr.puts "#{name} stack underflow"
+      $stderr.print "#{name} stack underflow\n"
     else
-      @s_out.print( @stack.pop )
+      @s_out.print(@stack.pop)
     end
   end
 
@@ -114,48 +120,50 @@ module PrimitiveWords
   end
 
   def dot_s(_name = nil)
-    @s_out.print( "<#{@stack.size}> #{@stack}\n" )
+    @s_out.print("<#{@stack.size}> #{@stack}\n")
   end
 
   def dot_d(_name)
     pp @dictionary
   end
-
 end
 
+# place holder
 class Dictionary
-  def initialize( &block )
+  def initialize(&block)
     @entries = {}
-    block.call( self ) if block
+    block&.call(self)
   end
 
-  def word( name, &block )
-    @entries[name] = { :name => name, :block => block, :immediate => false }
+  def word(name, &block)
+    @entries[name] = { name: name, block: block, immediate: false }
     self
   end
 
-  def immediate_word( name, &block )
-    @entries[name] = { :name => name, :block => block, :immediate => true }
+  def immediate_word(name, &block)
+    @entries[name] = { name: name, block: block, immediate: true }
     self
   end
 
-  def alias_word( name, old_name )
+  def alias_word(name, old_name)
     entry = self[old_name]
     raise "No such word #{old_name}" unless entry
+
     new_entry = entry.dup
     new_entry[:name] = name
     @entries[name] = new_entry
   end
 
-  def []( name )
+  def [](name)
     @entries[name]
   end
 end
 
+# place holder
 class RForth
   include PrimitiveWords
 
-  def initialize( s_in = $stdin, s_out = $stdout )
+  def initialize(s_in = $stdin, s_out = $stdout)
     @s_in = s_in
     @s_out = s_out
     @dictionary = Dictionary.new
@@ -167,67 +175,72 @@ class RForth
   def initialize_dictionary
     PrimitiveWords.public_instance_methods(false).each do |m|
       method_clojure = method(m.to_sym)
-      word( m.to_s, &method_clojure )
+      word(m.to_s, &method_clojure)
     end
 
-    alias_word( '?dup', 'q_dup' )
-    alias_word( '+', 'plus' )
-    alias_word( '*', 'mult' )
-    alias_word( '-', 'subtract' )
-    alias_word( '/', 'divide' )
-    alias_word( '.', 'dot' )
-    alias_word( '.S', 'dot_s' )
-    alias_word( '.D', 'dot_d' )
+    add_aliases
 
     word(':')     { read_and_define_word }
     word('bye')   { exit }
 
-    immediate_word( '\\' ) { @s_in.readline }
+    immediate_word('\\') { @s_in.readline }
+  end
+
+  def add_aliases
+    alias_word('?dup', 'q_dup')
+    alias_word('+', 'plus')
+    alias_word('*', 'mult')
+    alias_word('-', 'subtract')
+    alias_word('/', 'divide')
+    alias_word('.', 'dot')
+    alias_word('.S', 'dot_s')
+    alias_word('.D', 'dot_d')
   end
 
   # Convience method that takes a word and a closure
   # and defines the word in the dictionary
-  def word( name, &block )
-    @dictionary.word( name, &block )
+  def word(name, &block)
+    @dictionary.word(name, &block)
   end
 
   # Convience method that takes a word and a closure
   # and defines an immediate word in the dictionary
-  def immediate_word( name, &block )
-    @dictionary.immediate_word( name, &block )
+  def immediate_word(name, &block)
+    @dictionary.immediate_word(name, &block)
   end
 
   # Convience method that takes an existing dict.
   # word and a new word and aliases the new word to
   # the old.
-  def alias_word( name, old_name )
-    @dictionary.alias_word( name, old_name )
+  def alias_word(name, old_name)
+    @dictionary.alias_word(name, old_name)
   end
 
   # Given the name of a new words and the words
   # that make up its definition, define the
   # new word.
-  def define_word( name, *words )
-    @dictionary.word( name, &compile_words( *words ) )
+  def define_word(name, *words)
+    @dictionary.word(name, &compile_words(*words))
   end
 
   # Give an array of (string) words, return
   # A block which will run all of those words.
   # Executes all immedate words, well, immediately.
-  def compile_words( *words )
+  def compile_words(*words)
     blocks = []
+
     words.each do |word|
-      entry = resolve_word( word )
+      entry = resolve_word(word)
       raise "no such word: #{word}" unless entry
+
       if entry[:immediate]
         entry[:block].call(entry[:name])
       else
-        blocks << [ entry[:block], entry[:name] ]
+        blocks << [ entry[:block], entry[:name] ] # rubocop:disable Layout/SpaceInsideArrayLiteralBrackets
       end
     end
-    proc do
-      blocks.each {|b, n| b.call(n)}
-    end
+
+    proc { blocks.each { |b, n| b.call(n) } }
   end
 
   # Read a word definition from input and
@@ -242,10 +255,16 @@ class RForth
       exit 1
     end
 
+    words = read_definition
+    @dictionary.word(name, &compile_words(*words))
+  end
+
+  def read_definition
     words = []
 
     while (word = read_word)
       break if word == ';'
+
       words << word
     end
 
@@ -254,24 +273,28 @@ class RForth
       exit 1
     end
 
-    @dictionary.word(name, &compile_words( *words ))
+    words
   end
 
   # Given a (string) word, return the dictionary
   # entry for that word or nil.
-  def resolve_word( word )
+  def resolve_word(word)
     return @dictionary[word] if @dictionary[word]
+
     x = to_number(word)
+
     if x
       block = proc { @stack << x }
-      return { :name => word, :block => block, :immediate => false }
+      return { name: word, block: block, immediate: false }
     end
+
     nil
   end
 
   # Evaluate the given word.
-  def forth_eval( word )
+  def forth_eval(word)
     entry = resolve_word(word)
+
     if entry
       entry[:block].call(entry[:name])
     else
@@ -281,20 +304,24 @@ class RForth
 
   # Try to turn the word into a number, return nil if
   # conversion fails
-  def to_number( word )
+  def to_number(word)
     begin
-      return Integer( word )
-    rescue
+      return Integer(word)
+    rescue ArgumentError, FloatDomainError, Math::DomainError
+      # keep rubocop from complaining about suppressed exceptions
     end
+
     begin
-      return Float( word )
-    rescue
+      return Float(word)
+    rescue ArgumentError, FloatDomainError, Math::DomainError
+      # keep rubocop from complaining about suppressed exceptions
     end
+
     nil
   end
 
   def read_word
-    result = ''
+    result = String.new
 
     loop do
       begin
@@ -303,7 +330,7 @@ class RForth
         break
       end
 
-      if is_space?(ch)
+      if /\s/ =~ ch.chr
         break unless result.empty?
       else
         result << ch
@@ -313,18 +340,19 @@ class RForth
     result.empty? ? nil : result
   end
 
-  def is_space?( ch )
-    /\s/ =~ ch.chr
-  end
-
   def run
     loop do
       @s_out.flush
       word = read_word
       break if word.nil?
-      forth_eval( word )
+
+      forth_eval(word)
     end
   end
 end
+
+# rubocop:enable Metrics/ClassLength
+# rubocop:enable Metrics/MethodLength
+# rubocop:enable Metrics/ModuleLength
 
 RForth.new.run
